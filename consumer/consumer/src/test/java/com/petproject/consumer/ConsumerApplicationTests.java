694a45ff13328f99cda01749ff5da01ea1004288
StasMalikov <stas.malikov99@gmail.com>
2025-02-15 21:29:57 +0300

    init

---
+++ consumer/consumer/src/test/java/com/petproject/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
